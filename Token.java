face4e1826400b2259bd198f8aaf0d93e1bce034
Aditya Raj <76805326+AdityaRaj1440@users.noreply.github.com>
2021-08-21 10:02:04 +0530

    Update Token.java

--- Token.java
+++ Token.java
@@ -5,12 +5,12 @@ import java.awt.Graphics;
 
 public class Token 
 {
- private int x, y, score;
+ private int x, y, score;                                   //x: position of the token along X axis; y: position of token along Y axis; score: score after consuming each token.
  private Snake snake;
 
  public Token(Snake s)
  {
-	 x= (int)(Math.random()*495);
+	 x= (int)(Math.random()*495);                        //token must lie within the working area.
 	 y= (int)(Math.random()*395);
 	 snake= s;
  }
@@ -28,21 +28,21 @@ public class Token
  
  public void draw(Graphics g)
  {
-	 g.setColor(Color.yellow);
-	 g.fillRect(x, y, 6, 6);
-	 g.drawString(""+score,0,0);
+	 g.setColor(Color.yellow);                                      //to set the color of the token to yellow.
+	 g.fillRect(x, y, 6, 6);                                        //the token will be of rectangular shape having dimension 6*6
+	 //g.drawString(""+score,0,0);
  }
  
  public boolean snakeCollision()
  {
 	 int snakeX= snake.getHeadX()+2;
 	 int snakeY= snake.getHeadY()+2;
-	 if(snakeX>= x-1 && snakeX<=x+7)
+	 if(snakeX>= x-1 && snakeX<=x+7)                                 //to check whether the snake is coming in contact with the token or not.
 		 if(snakeY>= y-1 && snakeY<=y+7)
 		 {
-			 changePosition();
-			 score++;
-			 snake.setElongate(true);
+			 changePosition();                               //if the snake touches the token, then the latter is consumed a new token is to be generated elsewhere.
+			 score++;                                        //score is to be increased for each token consumed by the snake.
+			 snake.setElongate(true);                        //if token is consumed then, the snake can get elongated.
 			 return true;
 		 }
 	 return false;
